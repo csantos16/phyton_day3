671cdf77702fa99ff3ab8d65a4f76737bdd4e897
Santos <csantos16@cps.k12.il.us>
2024-08-30 09:24:29 -0500

    first commit

---
+++ phyton_day3/lesson2.py
@@ -0,0 +1,28 @@
+print("day 3 phyton programming")
+age = input("what is your age")
+print("Hello you are " + age + "years old")
+#concatenation is the process of joininh strings together
+#with a variable using the plus sign
+#ask for there home address
+home_address = input("what is your home address? ")
+#print out the home address of the user
+print("Hello you live at " + home_address)
+#ask for their phone number
+phone_number = input("What is your phone number? ")
+#print out the phone number of the user
+print("your phone number is " + phone_number)
+
+#ask for their email address
+#print out their email address
+
+#data types variables can store different types of data
+#strings are text data -- string data type
+name = "Cesar" #whenever you see text in quotes it is a string
+#integers are text data
+#integers are whole numbers
+age = 17 #no quotes around the number
+#floats are decimal numbers
+height= 5.5 #a number with a decimal point
+#boolean are true or false values
+is_student = True #True or False
+#lists are a collection of data
